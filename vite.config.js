4b678c1bb8e7e134a1a948d2d484e226c539c837
SkyVPS360 <shoppmonster@proton.me>
2025-03-10 01:01:46 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     cors: true,
     host: '0.0.0.0',
     hmr: {
-      clientPort: 443, // Force client port for proxied environments
+      clientPort: 5000, // Force client port for proxied environments
       host: 'skyvps360.xyz' // Allow HMR connections from your domain
     }
   },
